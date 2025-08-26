4f51e28507b353eb390408b7e742fb13f9164d5d
rubaiiyat <abirr1516@gmail.com>
2025-08-26 20:01:53 +0600

    add basic setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
-
+import Navbar from "./Components/Navbar/Navbar";
+import { Outlet } from "react-router";
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <nav>
+        <Navbar></Navbar>
+      </nav>
+      <main className="min-h-[calc(100vh-100px)] mx-24">
+        <Outlet></Outlet>
+      </main>
+      <footer>
+        <div className="text-4xl">This is footer</div>
+      </footer>
     </div>
   );
 }
