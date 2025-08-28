7311d54d85ff510ff53c8cd97cf37c7f2189de4b
rubaiiyat <abirr1516@gmail.com>
2025-08-28 17:52:17 +0600

    create add post api

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,58 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = process.env.MONGODB;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("potteryshop");
+    const itemCollection = database.collection("potteryitem");
+
+    app.post("/add-item", async (req, res) => {
+      const post = req.body;
+
+      try {
+        const result = await itemCollection.insertOne(post);
+        res.status(200).json({
+          message: "Item Added Successfull",
+          title: post.name,
+        });
+      } catch (error) {
+        res.status(404).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
